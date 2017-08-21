f941ef3774a04ffcba4cefc7ce7063df86dd584b
Friedrich <friedrich.zizmann@gmail.com>
2017-08-21 22:47:33 +0200

    language level geändert

--- src/test/java/de/thb/fz/dependency/DependencyLoaderTest.java
+++ src/test/java/de/thb/fz/dependency/DependencyLoaderTest.java
@@ -1,9 +1,13 @@
 package de.thb.fz.dependency;
 
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
+import org.reflections.Reflections;
 
 
 public class DependencyLoaderTest {
@@ -17,6 +21,26 @@ public class DependencyLoaderTest {
     );
     String testClass = "C:\\Users\\Friedrich\\git\\masterarbeit\\target\\test-classes\\de\\thb\\fz\\dependency\\DependencyLoaderTest.class";
     Assert.assertTrue(list.stream().anyMatch(item -> testClass.equals(item)));
+  }
+
+  @Test
+  public void reflectiveClassList() throws Exception {
+    Reflections reflections = new Reflections("de.thb.fz");
+
+    Set<Class<? extends Object>> allClasses =
+        reflections.getSubTypesOf(Object.class);
+    System.out.println("test");
+  }
+
+  @Test
+  public void fast() throws ClassNotFoundException {
+    List<String> fast = new FastClasspathScanner("junit.framework")
+        .scan()
+        .getNamesOfAllClasses();
+    for (String s : fast) {
+      Class cls = Class.forName(s);
+      System.out.println(cls.getName());
+    }
   }
 
 }
