70eef0a7f13ea246555a603f78513ac8bcda1068
Friedrich <friedrich.zizmann@gmail.com>
2017-10-08 14:15:41 +0200

    final final

--- src/main/java/de/thb/fz/maven/ArchitectureMojo.java
+++ src/main/java/de/thb/fz/maven/ArchitectureMojo.java
@@ -5,31 +5,49 @@ import de.thb.fz.dependency.DependencyLoader;
 import de.thb.fz.dsl.Architecture;
 import de.thb.fz.dsl.ArchitectureDescription;
 import de.thb.fz.dsl.builder.ArchitectureBuilder;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "analyze", defaultPhase = LifecyclePhase.TEST)
+@Mojo(name = "analyze", defaultPhase = LifecyclePhase.TEST, requiresDependencyResolution = ResolutionScope.TEST)
 public class ArchitectureMojo extends AbstractMojo {
 
   @Parameter(property = "architectureClass", required = true)
   private String architectureClass;
   @Parameter(property = "basePackage", required = true)
   private String basePackage;
-  @Parameter(property = "strictInterface", required = false)
-  private boolean strictInterface = false;
+  @Parameter(property = "strictInterface")
+  private boolean strictInterface;
 
   public void execute() throws MojoExecutionException {
     ArchitectureAnalyser architectureAnalyser = new ArchitectureAnalyser();
     try {
-      Class architectureClass = Class.forName(this.architectureClass);
-      Object objectInstance = architectureClass.newInstance();
-      if (objectInstance instanceof ArchitectureDescription) {
-        Architecture architecture = ((ArchitectureDescription) objectInstance).defineArchitecture();
+      List runtimeClasspathElements = ((MavenProject) this.getPluginContext().get("project"))
+          .getRuntimeClasspathElements();
+
+      URL[] runtimeUrls = new URL[runtimeClasspathElements.size()];
+      for (int i = 0; i < runtimeClasspathElements.size(); i++) {
+        String element = (String) runtimeClasspathElements.get(i);
+        runtimeUrls[i] = new File(element).toURI().toURL();
+      }
+
+      URLClassLoader newLoader = new URLClassLoader(runtimeUrls,
+          Thread.currentThread().getContextClassLoader());
+      Object architectureClass = newLoader.loadClass(this.architectureClass).newInstance();
+
+      if (architectureClass instanceof ArchitectureDescription) {
+        Architecture architecture = ((ArchitectureDescription) architectureClass)
+            .defineArchitecture();
         new ArchitectureBuilder(new DependencyLoader()).accumulateArchitecture(architecture);
 
         getLog().info("Komponentenverletzungen:");
