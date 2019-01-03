306f16923eddb068157f7e6d8b2bc093ffb142dd
f.zizmann@clinpath.de <d2YRtry77BnE>
2019-01-03 21:49:58 +0100

    testsuite added

---
+++ src/test/java/de/thb/fz/asm/ByteCodeSuite.java
@@ -0,0 +1,14 @@
+package de.thb.fz.asm;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    ByteCodeTest.class
+})
+public class ByteCodeSuite
+{
+
+}
